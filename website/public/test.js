9fc61c9529d67e1801e000c85a8e68192c093fa7
Gavin Chu <gchu@gchu-mn1.linkedin.biz>
2014-07-12 03:06:47 -0700

    pins added to gavin

--- website/public/test.js
+++ website/public/test.js
@@ -309,20 +309,47 @@ var PostItemView = Parse.View.extend({
 
 var postItemView = new PostItemView();
 
-// Main page of items view
+
+const lat_ca = 37.3;
+const lng_ca = -119.4;
+
+function loadGavinMap(){
+  console.log('loadGavinMap');
+  var options = setupMapOptions(lat_ca, lng_ca);
+  loadMap(options, "gavin");
+}
+
 var MapView = Parse.View.extend({
   el: '.page',
-  render: function() {
+  render: function(type) {
     var template = _.template($('#map-template').html(), {});
     this.$el.html(template);
     var map;
-    google.maps.event.addDomListener(window, 'load', loadMap);
+    if(type == "gavin")
+      google.maps.event.addDomListener(window, 'load', loadGavinMap);
+    else if(type == "maggie")
+      google.maps.event.addDomListener(window, 'load', loadMaggieMap);
+    else if(type == "all")
+      google.maps.event.addDomListener(window, 'load', loadAllMap);
   }
 });
 
+function setupMapOptions(lat, lng) {
+  var mapOptions = {
+    center : new google.maps.LatLng(lat, lng),
+    zoom : 6,
+    maxZoom : 15,
+    minZoom : 2,
+    //Turn off the map controls as we will be adding our own later.
+    panControl: false,
+    mapTypeControl: false
+  }
+  return mapOptions;
+}
+
 var mapCenter = new google.maps.LatLng(37.386052, -122.083851);
 //The degree to which the map is zoomed in. This can range from 0 (least zoomed) to 21 and above (most zoomed).
-var mapZoom = 10;
+var mapZoom = 8;
 //The max and min zoom levels that are allowed.
 var mapZoomMax = 15;
 var mapZoomMin = 6;
@@ -340,52 +367,121 @@ var mapOptions = {
   mapTypeControl: false
 };
 
-function loadMap() {
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  loadMapMarkers();
+function loadMap(options, type) {
+  console.log('loadMap ' + type);
+  map = new google.maps.Map(document.getElementById("map"), options);
+  loadMapMarkers(type);
 }
 
-function loadMapMarkers () {
-  var markerPosition = new google.maps.LatLng(37.386052, -122.083851);
-  var markerIcon = {
-    url: 'images/icon_green2.png',
-    //The size image file.
-    scaledSize: new google.maps.Size(200, 150),
-    //The point on the image to measure the anchor from. 0, 0 is the top left.
+const lat_berkeley = 37.871593;
+const lng_berkeley = -122.272747;
+const lat_la = 34.052234;
+const lng_la = -118.243685;
+const lat_vegas = 36.169941;
+const lng_vegas = -115.13983;
+
+function loadMapMarkers(type) {
+  if(type == "gavin")
+    loadGavinMarkers();
+  else if(type == "maggie")
+    loadMaggieMarkers();
+  else if(type == "all")
+    loadAllMarkers();
+}
+
+function loadGavinMarkers(){
+  console.log('loadGavinMarkers');
+  var berkeleyPosition = new google.maps.LatLng(lat_berkeley, lng_berkeley);
+  var berkeleyIcon = {
+    url: 'images/pin_norcal.png',
+    scaledSize: new google.maps.Size(140, 200),
     origin: new google.maps.Point(0, 0),
-    //The x y coordinates of the anchor point on the marker. e.g. If your map marker was a drawing pin then the anchor would be the tip of the pin.
-    anchor: new google.maps.Point(200, 150)
+    anchor: new google.maps.Point(70, 200)
   };
-
-  //Setting the shape to be used with the Glastonbury map marker.
-  var markerShape = {
-    coord: [12, 4, 216, 22, 212, 74, 157, 70, 184, 111, 125, 67, 6, 56],
-    type: 'poly'
-  };
-
-  //Creating the Glastonbury map marker.
-  marker = new google.maps.Marker({
-    //uses the position set above.
-    position: markerPosition,
-    //adds the marker to the map.
+  berkeleyMarker = new google.maps.Marker({
+    position: berkeleyPosition,
     map: map,
-    title: 'title',
-    //assigns the icon image set above to the marker.
-    icon: markerIcon,
-    //assigns the icon shape set above to the marker.
-    shape: markerShape,
-    //sets the z-index of the map marker.
+    title: 'berkeley',
+    icon: berkeleyIcon,
+//    shape: markerShape,
+    zIndex: 101
+  });
+
+  var laPosition = new google.maps.LatLng(lat_la, lng_la);
+  var laIcon = {
+    url: 'images/pin_socal.png',
+    scaledSize: new google.maps.Size(80, 112),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(40, 112)
+  };
+  laMarker = new google.maps.Marker({
+    position: laPosition,
+    map: map,
+    title: 'los angeles',
+    icon: laIcon,
+    zIndex: 102
+  });
+
+  var vegasPosition = new google.maps.LatLng(lat_vegas, lng_vegas);
+  var vegasIcon = {
+    url: 'images/pin_vegas.png',
+    scaledSize: new google.maps.Size(80, 112),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(40, 112)
+  };
+  vegasMarker = new google.maps.Marker({
+    position: vegasPosition,
+    map: map,
+    title: 'vegas',
+    icon: vegasIcon,
     zIndex: 102
   });
 }
+
+//function loadMapMarkers () {
+//  var markerPosition = new google.maps.LatLng(37.386052, -122.083851);
+//  var markerIcon = {
+//    url: 'images/icon_green2.png',
+//    //The size image file.
+//    scaledSize: new google.maps.Size(140, 200),
+//    //The point on the image to measure the anchor from. 0, 0 is the top left.
+//    origin: new google.maps.Point(0, 0),
+//    //The x y coordinates of the anchor point on the marker. e.g. If your map marker was a drawing pin then the anchor would be the tip of the pin.
+//    anchor: new google.maps.Point(200, 150)
+//  };
+//
+//  //Setting the shape to be used with the Glastonbury map marker.
+//  var markerShape = {
+//    coord: [12, 4, 216, 22, 212, 74, 157, 70, 184, 111, 125, 67, 6, 56],
+//    type: 'poly'
+//  };
+//
+//  //Creating the Glastonbury map marker.
+//  marker = new google.maps.Marker({
+//    //uses the position set above.
+//    position: markerPosition,
+//    //adds the marker to the map.
+//    map: map,
+//    title: 'title',
+//    //assigns the icon image set above to the marker.
+//    icon: markerIcon,
+//    //assigns the icon shape set above to the marker.
+//    shape: markerShape,
+//    //sets the z-index of the map marker.
+//    zIndex: 102
+//  });
+//}
 
 var mapView = new MapView();
 
 var Router = Parse.Router.extend({
 	routes: {
-	   	"": "home",
-	   	"edit/:id": "post",
-	  	"post": "post",
+	  "": "home",
+    "gavin" : "home",
+    "maggie" : "maggie",
+    "all" : "all",
+	  "edit/:id": "post",
+	  "post": "post",
 		"login" : "login",
 		"viewItem/:id" : "viewItem",
 		"items" : "viewMyItems"
@@ -394,7 +490,15 @@ var Router = Parse.Router.extend({
 
 var router = new Router();
 router.on("route:home", function() {
-	mapView.render();
+	mapView.render("gavin");
+});
+
+router.on("route:maggie", function() {
+  mapView.render("maggie");
+});
+
+router.on("route:all", function() {
+  mapView.render("all");
 });
 
 router.on("route:post", function() {
